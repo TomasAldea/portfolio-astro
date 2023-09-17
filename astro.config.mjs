276f318e3ad5ada1d3f86c93a6286f4719262cf6
TomasAldea <tomas.sbd.5@gmail.com>
2023-09-17 18:11:28 +0200

    base url

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://tomasaldea.github.io'
+    site: 'https://tomasaldea.github.io',
+    base: '/portfolio-astro',
 });
