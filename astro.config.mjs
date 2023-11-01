4be9d0de958a654a6b9748a549cf1b3427dd6e64
TomasAldea <tomas.sbd.5@gmail.com>
2023-11-01 13:11:06 +0100

    scroll to

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig,passthroughImageService } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
     site: 'https://tomasaldea.github.io',
     base: '/portfolio-astro',
+    image: {
+        service: passthroughImageService(),
+    },
 });
